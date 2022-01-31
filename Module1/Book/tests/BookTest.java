0b74dd5773ae1e401fb8da7d7abdeadeb7102e92
KeChen97 <kechen@wustl.edu>
2022-01-31 01:04:47 -0800

    Lab01

---
+++ Module1/Book/tests/BookTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A JUnit test class for the Book class
+ */
+public class BookTest {
+
+  private Person author;
+  private Book harryPotter;
+
+
+  @Before
+  public void setUp(){
+    this.author  = new Person("J.K.","Rowling",1965);
+    this.harryPotter = new Book ("Harry Potter", this.author, 24.99f);
+  }
+
+  @Test
+  public void testTitle(){
+    assertEquals("J.K.", this.harryPotter.getTitle());
+  }
+
+  @Test
+  public void testAuthor(){
+    assertEquals(this.author,this.harryPotter.getAuthor());
+  }
+
+  @Test
+  public void testPrice(){
+    assertEquals(24.99f,this.harryPotter.getPrice());
+  }
+}

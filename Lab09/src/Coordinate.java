53dc45094025c3389c21d40ecb4a1ea4e575cba5
KeChen97 <kechen@wustl.edu>
2022-04-06 21:37:44 -0700

    Lab 09

---
+++ Lab09/src/Coordinate.java
@@ -0,0 +1,30 @@
+public class Coordinate {
+
+  /**
+   * Constructor of Coordinate
+   * @param x
+   * @param y
+   */
+  public Coordinate(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  int x;
+  int y;
+
+  /**
+   * A function check if two coordinate are equal
+   * @param obj another object to be compared with this coordinate
+   * @return true if it is also a Coordinate object and has same x and y
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Coordinate) {
+      Coordinate other = (Coordinate) obj;
+      return other.x == this.x && other.y == this.y;
+    }
+
+    return false;
+  }
+}

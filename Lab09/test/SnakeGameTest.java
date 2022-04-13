354036983574f552535d0616e017c7b3385f89fa
KeChen97 <kechen@wustl.edu>
2022-04-13 12:36:08 -0700

    Lab 09

---
+++ Lab09/test/SnakeGameTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+/**
+ * Since it is a graphic game, there is nothing returned can be checked/returned.
+ * A. We can use some GUI testing Interface to test the functions in this file
+ * For example:
+ * ComponentTestFixture automatically handles disposal of GUI components
+ * We can write tests extend this.
+ * public class ArrowButtonTest extends ComponentTestFixture {}
+ *
+ * B. Or Manual Based Testing
+ * we can visually check:
+ * 1. will the snake move correctly based on controller's keyboard actions?
+ * 2. will the snake move smoothly if the controller doesn't do anything?
+ * 3. Can the apple be "eaten" once the snake touches it?
+ * 4. Will the apple appear in random location everytime?
+ */
+public class SnakeGameTest {
+
+}
